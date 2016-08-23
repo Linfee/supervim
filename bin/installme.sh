5cd284966da7d62155aa7620981c4134299d2889
Linfee <linfee@hotmail.com>
2016-08-23 20:47:55 +0800

    添加自己安装的脚本

---
+++ bin/installme.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+rm -rf ~/.vim ~/.vimrc
+
+mkdir ~/.vim
+
+git clone git@github.com:Linfee/supervim.git ~/.vim/supervim
+
+curl -fLo ~/.vim/supervim/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo 'set runtimepath+=~/.vim
+
+source ~/.vim/supervim/basic.vim
+
+try
+    source ~/.vim/custom.vim
+catch
+endtry' > ~/.vimrc
+
+echo "Done"
+
+mkdir ~/.vim/temp
+
+vim -u ~/.vim/supervim/basic.vim -c 'call Init()'
+
+cd ~/.vim/plugged/vimproc.vim
+
+make
+
+echo '
+colorscheme molokai
+' >> ~/.vim/custom.vim
